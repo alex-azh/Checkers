a0ae88ca8430f2bbe52289e7163f636c56fca964
Aleksandr Azhgihin <aleksandr.azhgihin@uralkali.com>
2023-11-01 13:45:39 +0500

    Добавил результаты теста

--- Chess2/Benchs/BenchTests.cs
+++ Chess2/Benchs/BenchTests.cs
@@ -1,5 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
-
+/*
+| Method              | Mean     | Error     | StdDev    | Gen0   | Allocated |
+|-------------------- |---------:|----------:|----------:|-------:|----------:|
+| TestKills1Struct    | 1.259 us | 0.8436 us | 0.0462 us | 0.2804 |    2.3 KB |
+| TestKills2Struct    | 3.025 us | 0.1849 us | 0.0101 us | 0.6218 |   5.09 KB |
+| TestKills3Struct    | 2.964 us | 0.7900 us | 0.0433 us | 0.5760 |   4.73 KB |
+| TestVariants1Struct | 1.268 us | 0.1305 us | 0.0072 us | 0.3338 |   2.73 KB |
+ */
 namespace Chess2.Benchs;
 
 [ShortRunJob]
