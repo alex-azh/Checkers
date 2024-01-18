347ded29e4565482fb0d26108a865ab6d080c38a
Suvorova_YS <Suvorova_YS@N072398>
2024-01-18 16:13:48 +0500

    Board.GetBestMove

--- Chess2/Board.cs
+++ Chess2/Board.cs
@@ -4,6 +4,8 @@ using System.Numerics;
 namespace CheckersGame;
 public record Board(uint WhiteP, uint WhiteD, uint BlackP, uint BlackD)
 {
+    const int countBestMoves = 3;
+    const float depthFactor = 0.0001f;
     public static uint StartWhites => 0b00_00000_00000_00000_00011_11111_11111;
     public static uint StartBlacks => 0b11_11111_11111_00000_00000_00000_00000;
     public static Board NewBoard() => new(StartWhites, 0, StartBlacks, 0);
@@ -49,5 +51,69 @@ public record Board(uint WhiteP, uint WhiteD, uint BlackP, uint BlackD)
             result[i + 96] = (BlackD >> i & 0x01) > 0;
         }
         return result;
+    }
+
+    public (Board board, float mark) GetBestMove(float depth)
+    {
+        (Board rBoard, float mark) best = GetBestAndRndMoves(countBestMoves).Select(board =>
+        {
+            var moves = board.board.Flip().GetBestAndRndMoves(countBestMoves).ToList();
+            var amount = moves.Count;
+            //в оригинале было так: (т.е. возвращалось ((board, mark), mark) )
+            //return (board, mark: board.board.Flip().GetBestAndRndMoves(countBestMoves)
+            return (board.board, mark: moves
+            .Select(rBoard => depth > depthFactor ? rBoard.board.GetBestMove(depth / amount).mark : rBoard.mark)
+            .DefaultIfEmpty(board.board.BlackP == 0 ? -1f : 0)
+            .Max());
+        })
+            .MinBy(p => p.mark);
+        //в оригинале:
+        //return (best.board.rBoard, best.mark)
+        return best;
+    }
+
+
+    IEnumerable<(Board board, float mark)> GetBestAndRndMoves(int count)
+    {
+        /*
+         * у нас Moves() возвращает кортеж (board, hasKill)
+         * Саша, я так понимаю, что нам этот hasKill нужен, чтобы понимать, продолжит ли ход игрок
+         * но мы же можем уже после выбора лучшего хода определить, было ли убийство просто сравнив исходное положение противника
+         * и его положение в выбранном лучшем ходе, тогда нам не нужно это тут таскать
+        */
+        SortedSet<(Board board, float mark, float rnd)> bestMoves = new(Comparer<(Board, float, float)>.Create((x,y) => x.Item2.CompareTo(y.Item2)));
+        SortedSet<(Board board, float mark, float rnd)> rndMoves = new(Comparer<(Board, float, float)>.Create((x, y) => x.Item3.CompareTo(y.Item3)));
+
+        var moves = Moves().Chunk(100).SelectMany(chunk => (chunk.Select(c => c.board).Zip(Evaluate(chunk.Select(c => c.board).ToArray()))));
+        //в оригинале было так: (т.к. Moves() просто коллекцию досок возвращал)
+        //var moves = Moves().Chunk(100).SelectMany(chunk => chunk.Zip(Evaluate(chunk)));
+
+        foreach (var move in moves)
+        {
+            var rnd = Random.Shared.NextSingle();
+            if (bestMoves.Count < count)
+                bestMoves.Add((move.First, move.Second, rnd));
+            else if(move.Second > bestMoves.Min.mark)
+            {
+                bestMoves.Remove(bestMoves.Min);
+                bestMoves.Add((move.First, move.Second, rnd));
+            }
+            
+            if (rndMoves.Count < count + 1)
+                rndMoves.Add((move.First, move.Second, rnd));
+            else if (rnd > rndMoves.Min.rnd)
+            {
+                rndMoves.Remove(rndMoves.Min);
+                rndMoves.Add((move.First, move.Second, rnd));
+            }
+        }
+        rndMoves.ExceptWith(bestMoves);
+        return bestMoves.Select(m => (m.board,m.mark)).Concat(rndMoves.Select(m => (m.board, m.mark)).Take(1));
+    }
+
+    float[] Evaluate(Board[] moves)
+    {
+        //здесь вычисление оценок ходов (нейросеть)
+        return moves.Select(m => Random.Shared.NextSingle()).ToArray(); //пока массив рандомных оценок
     }
 }
