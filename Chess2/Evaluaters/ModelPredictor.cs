228ae452dd096440067a83befd496a03391a25cd
Suvorova_YS <Suvorova_YS@N072398>
2024-02-05 17:37:25 +0500

    Надо о ModelPredictor подумать

--- Chess2/Evaluaters/ModelPredictor.cs
+++ Chess2/Evaluaters/ModelPredictor.cs
@@ -11,16 +11,20 @@ namespace CheckersGame.Evaluaters;
 public class ModelPredictor : IPredictor
 {
     private Sequential _sequential;
+    private OptimizerHelper _optimizer;
     public ModelPredictor()
     {
         // load model
         _sequential = Sequential(
             ("inputLayer", Linear(128, 64)),
             ("func", Sigmoid()),
-            //("hidden1", Linear(64, 64)),
-            //("func", Sigmoid()),
-            ("output", Linear(16, 1))
+            ("hidden1", Linear(64, 16)),
+            ("func", Sigmoid()),
+            ("output", Linear(16, 1)),
+            ("func", Sigmoid())
             );
+        double learningRate = 0.1; // Чем он выше, тем сильнее агент доверяет новой информации
+        _optimizer = torch.optim.Adam(_sequential.parameters(), learningRate);
     }
     public ModelPredictor(string modelPath) : this()
     {
@@ -66,4 +70,39 @@ public class ModelPredictor : IPredictor
             return (moves.ToArray(), gamesResults.ToArray());
         }
     }
+    public void Educate(float[][] array, float reward)
+    {
+
+        float discountFactor = 0.9f; // Чем он меньше, тем меньше агент предпочитает выгоду от своих будущих действий в пользу настоящего
+        _optimizer.zero_grad();
+
+        float[] input = array.SelectMany(x => x.Select(x => x)).ToArray();
+        torch.Tensor tensor = torch.from_array(input).reshape(array.Length, 128);
+        torch.Tensor eval = _sequential.forward(tensor);
+
+        torch.Tensor target = torch.from_array(GetTarget());
+
+        var loss = functional.mse_loss(eval, target, Reduction.Sum);
+        loss.backward();
+        _optimizer.step();
+        _sequential.save("D:\\AI\\model.pt");
+
+        float[] GetTarget()
+        {
+            float df = 0.7f;
+            // текущие оценки шагов
+            float[] current = eval.data<float>().ToArray();
+
+            float[] target = new float[current.Length];
+            float next = reward;
+            for (int i = current.Length - 1; i >= 0; i--)
+            {
+                target[i] = current[i] + (next - current[i]) * df;
+                next = target[i];
+            }
+
+            return target;
+        }
+    }
 }
+
